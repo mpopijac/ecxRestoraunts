aab7a26c15a0a5faddf2a74c0beb5148085067db
mpopijac <matija.popijac@hotmail.com>
2017-02-10 22:59:45 +0100

    Controller and the index page is created

---
+++ src/main/java/ecx/mpopijac/restaurants/controllers/IndexController.java
@@ -0,0 +1,27 @@
+package ecx.mpopijac.restaurants.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ecx.mpopijac.restaurants.models.Article;
+import ecx.mpopijac.restaurants.service.ArticleService;
+
+@Controller
+public class IndexController {
+
+	@Autowired
+	ArticleService articleService;
+
+	@RequestMapping(value = "/*", method = RequestMethod.GET)
+	public String indexPage(Model model) {
+		List<Article> articles = articleService.findAll();
+		model.addAttribute("articles", articles);
+		return "index";
+	}
+
+}
